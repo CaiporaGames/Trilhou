18ecfb0444a2eee08f1ced4437a6aa2380e7b17b
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-08-03 17:12:27 -0300

    Text and flashlight
    I implemented the fleshlight and the text for player

---
+++ Trilhou/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] SODialogue dialogues;
+    [SerializeField] TMP_Text dialogueText;
+
+    [Tooltip("This holds the current sentence index on the sentences string list")]
+    int setenceIndex;
+
+    private void Start()
+    {
+        DisplayDialogue(0);
+    }
+
+    void DisplayDialogue(int setenceIndex)
+    {
+        dialogueText.text = dialogues.setences[setenceIndex].setences;
+    
+    }
+
+    public void NextDialogue()
+    {
+        if (setenceIndex < dialogues.setences.Length - 1)
+        {
+                setenceIndex++;
+            DisplayDialogue(setenceIndex);
+        }
+    }
+
+    public void BackDialogue()
+    {
+        if (setenceIndex > 0)
+        {
+            setenceIndex--;
+            DisplayDialogue(setenceIndex);
+        }       
+    }
+
+}
---
+++ Trilhou/Assets/Scripts/SODialogue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject / Dialogue", fileName = "Dialogue")]
+public class SODialogue : ScriptableObject
+{
+    public Dialogue[] setences;
+}
+
+[Serializable]
+public class Dialogue
+{
+    [TextArea(3,10)]
+    public string setences;
+    public string names;
+}
