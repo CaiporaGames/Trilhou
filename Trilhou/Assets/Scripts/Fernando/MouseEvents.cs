ac0cb5fd5f1a600efafed10ffc86ada0dfa53883
Luis Fernando <luisff@dcomp.ufs.br>
2021-09-16 20:56:35 -0300

    importando scene com os baloes

---
+++ Trilhou/Assets/Scripts/Fernando/MouseEvents.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        Debug.Log("Entrou!");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Saiu!");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicou!");
+    }
+}
