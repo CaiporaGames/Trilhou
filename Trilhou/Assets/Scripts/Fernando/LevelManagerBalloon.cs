06aecc8415f38def271f8dc0210a3507a254fed9
Luis Fernando <luisff@dcomp.ufs.br>
2021-09-30 20:19:32 -0300

    corrigir bugs

--- Trilhou/Assets/Scripts/Fernando/LevelManagerBalloon.cs
+++ Trilhou/Assets/Scripts/Fernando/LevelManagerBalloon.cs
@@ -3,62 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LevelManagerBalloon : MonoBehaviour
+namespace Fernando
 {
-    public static LevelManagerBalloon levelManager;
-
-    private int countBalloon = 0;
-    private bool gameOver = false;
-
-    private float seconds;
-    private int secondsToInt;
-    private int minutes;
-
-
-    public Text minutesText;
-    public Text secondsText;
-    public Text balloonText;
-
-    void Awake()
+    public class LevelManagerBalloon : MonoBehaviour
     {
-        if (levelManager == null)
-        {
-            levelManager = this;
-        }
-        else if (levelManager != this)
-        {
-            Destroy(gameObject);
-        }
+        public static LevelManagerBalloon levelManager;
 
-    }
+        private int countBalloon = 0;
+        private bool gameOver = false;
+
+        private float seconds;
+        private int secondsToInt;
+        private int minutes;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!gameOver)
+        public Text minutesText;
+        public Text secondsText;
+        public Text balloonText;
+
+        void Awake()
         {
-            seconds += Time.deltaTime;
-            if (seconds >= 60)
+            if (levelManager == null)
             {
-                seconds = 0;
-                minutes++;
-                minutesText.text = minutes.ToString();
+                levelManager = this;
             }
-            secondsToInt = (int)seconds;
-            secondsText.text = secondsToInt.ToString();
+            else if (levelManager != this)
+            {
+                Destroy(gameObject);
+            }
+
         }
 
-    }
 
-    public void SetBaloes()
-    {
-        countBalloon++;
-        balloonText.text = countBalloon.ToString();
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (!gameOver)
+            {
+                seconds += Time.deltaTime;
+                if (seconds >= 60)
+                {
+                    seconds = 0;
+                    minutes++;
+                    minutesText.text = minutes.ToString();
+                }
+                secondsToInt = (int)seconds;
+                secondsText.text = secondsToInt.ToString();
+            }
 
-    public int GetBaloes()
-    {
-        return countBalloon;
+        }
+
+        public void SetBaloes()
+        {
+            countBalloon++;
+            balloonText.text = countBalloon.ToString();
+        }
+
+        public int GetBaloes()
+        {
+            return countBalloon;
+        }
     }
 }
--- Trilhou/Assets/Scripts/Fernando/Move2D.cs
+++ Trilhou/Assets/Scripts/Fernando/Move2D.cs
@@ -1,53 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Move2D : MonoBehaviour
+namespace Fernando
 {
-    // Start is called before the first frame update
-    public Rigidbody2D rb;
-    public int moveSpeed;
-    private float direction;
 
-    private Vector3 facingRight;
-    private Vector3 facingLeft;
 
-    public bool inFloor; // em contato com o chao
-    public Transform detectFloor; // detectar o chao
-    public LayerMask isFloor; // identificar o chao
-
-    void Start()
+    public class Move2D : MonoBehaviour
     {
-        facingRight = transform.localScale;
-        facingLeft = transform.localScale;
-        facingLeft.x = facingLeft.x * -1;
+        // Start is called before the first frame update
+        public Rigidbody2D rb;
+        public int moveSpeed;
+        private float direction;
 
-        rb = GetComponent<Rigidbody2D>();
-    }
+        private Vector3 facingRight;
+        private Vector3 facingLeft;
 
-    // Update is called once per frame
-    void Update()
-    {
-        inFloor = Physics2D.OverlapCircle(detectFloor.position, 0.2f, isFloor);
+        public bool inFloor; // em contato com o chao
+        public Transform detectFloor; // detectar o chao
+        public LayerMask isFloor; // identificar o chao
 
-        if (Input.GetButtonDown("Jump") && inFloor == true)
+        void Start()
         {
-            rb.velocity = Vector2.up * 12;
+            facingRight = transform.localScale;
+            facingLeft = transform.localScale;
+            facingLeft.x = facingLeft.x * -1;
+
+            rb = GetComponent<Rigidbody2D>();
         }
 
-        direction = Input.GetAxis("Horizontal");
-
-        if (direction > 0)
+        // Update is called once per frame
+        void Update()
         {
-            //olhando para a direita
-            transform.localScale = facingRight;
-        }
-        if (direction < 0)
-        {
-            //olhando para a esquerda
-            transform.localScale = facingLeft;
-        }
+            inFloor = Physics2D.OverlapCircle(detectFloor.position, 0.2f, isFloor);
 
-        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+            if (Input.GetButtonDown("Jump") && inFloor == true)
+            {
+                rb.velocity = Vector2.up * 12;
+            }
+
+            direction = Input.GetAxis("Horizontal");
+
+            if (direction > 0)
+            {
+                //olhando para a direita
+                transform.localScale = facingRight;
+            }
+            if (direction < 0)
+            {
+                //olhando para a esquerda
+                transform.localScale = facingLeft;
+            }
+
+            rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+        }
     }
 }
