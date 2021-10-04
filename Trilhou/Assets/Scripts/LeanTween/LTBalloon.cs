910e8001401eff04f5944fa4b3e0076ede996dff
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-10-03 21:54:01 -0300

    Balloon Scene
    First setup for the balloon scene

---
+++ Trilhou/Assets/Scripts/LeanTween/LTBalloon.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LTBalloon : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] float xtime;
+    [SerializeField] float yPosition;
+
+
+    SpriteRenderer sprite;
+    private void Awake()
+    {
+        sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        ChooseColor();
+        ChooseSize();
+    }
+    void Start()
+    {
+        int delay = Random.Range(0,4);
+        int randomX = Random.Range(-5, 6);
+        float ytime = Random.Range(5, 20);
+        LeanTween.moveY(gameObject, yPosition, ytime).setEase(curve).setDelay(delay);
+        LeanTween.moveX(gameObject, randomX, xtime).setEase(curve).setLoopPingPong();
+    }
+
+    void ChooseColor()
+    {
+        int i = Random.Range(0, 7);
+
+        switch (i)
+        {
+            case 1:
+                sprite.material.color = Color.red;
+                break;
+            case 2:
+                sprite.material.color = Color.green;
+                break;
+            case 3:
+                sprite.material.color = Color.blue;
+                break;
+            case 4:
+                sprite.material.color = Color.yellow;
+                break;           
+            case 5:
+                sprite.material.color = Color.white;
+                break;
+            case 6:
+                sprite.material.color = Color.cyan;
+                break;
+            default:
+                sprite.material.color = Color.cyan;
+                break;
+        }
+    }
+
+    void ChooseSize()
+    {
+        int i = Random.Range(0,5);
+        switch (i)
+        {
+            case 1:
+                transform.GetChild(0).localScale = new Vector3(0.2f, 0.2f, 1);
+                break;
+            case 2:
+                transform.GetChild(0).localScale = new Vector3(0.2f, 0.3f, 1);
+                break;
+            case 3:
+                transform.GetChild(0).localScale = new Vector3(0.3f, 0.2f, 1);
+                break;
+            case 4:
+                transform.GetChild(0).localScale = new Vector3(0.3f, 0.3f, 1);
+                break;
+            default:
+                transform.GetChild(0).localScale = new Vector3(0.2f, 0.2f, 1);
+                break;
+        }
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
