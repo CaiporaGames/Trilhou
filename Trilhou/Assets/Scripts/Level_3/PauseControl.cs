8c434d2ea2709e60986df5cc35b5629c3c67cbfd
Luis Fernando <luisff@dcomp.ufs.br>
2021-10-07 18:24:01 -0300

    incluir script de pausar jogo

---
+++ Trilhou/Assets/Scripts/Level_3/PauseControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+
+    bool isPause;
+
+    private void Pause()
+    {
+        Time.timeScale = 0;        
+    }
+
+    private void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPause = !isPause;
+            if (isPause)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+        
+    }
+}
